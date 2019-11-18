55a9db907dbe5662c89d5d5b5be4bd38fb16d9e8
rafacazal <rafaelacazal12@gmail.com>
2019-11-18 18:00:03 -0200

    adicionei a pasta semana4

---
+++ semana4/aula1-funcoes/planner/script.js
@@ -0,0 +1,13 @@
+
+
+
+function criarTarefa(){ 
+const NomeTarefa = document.getElementById('novaTask').value
+getElementById("novaTask").innerHTML += NomeTarefa
+
+const DiasDaSemana = document.getElementsByClassName('content').value
+getElementById('content').innerHTML += DiasDaSemana
+
+}
+
+
