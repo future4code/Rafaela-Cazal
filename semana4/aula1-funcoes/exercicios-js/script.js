d242979ab039f7ff1eb1b4b5c1b34d1e3863cf7e
rafacazal <rafaelacazal12@gmail.com>
2019-11-20 21:22:37 -0200

    exercicios js

---
+++ semana4/aula1-funcoes/exercicios-js/script.js
@@ -0,0 +1,19 @@
+
+// 2.
+
+// const ArraydeNumeros = [1,2,3,4]
+
+// for (let i = 0, A = ArraydeNumeros.length; i < A; i++) {
+//       console.log(ArraydeNumeros[i]);
+//  }
+
+// 4.
+
+// hello = () => {
+
+//     return alert("Hello Future4");
+//   }
+  
+// hello()
+
+// 5.
