0ba599217386b0ed037f3cd2566ac4e354e64d31
rafacazal <rafaelacazal12@gmail.com>
2019-11-21 22:50:26 -0200

    pendente array

--- semana4/aula3-classes/blog/script.js
+++ semana4/aula3-classes/blog/script.js
@@ -1,5 +1,20 @@
-function onKeyDown(ev) {
-    if (ev.key === "Enter") {
-        aoCriar()
+class Post {
+    constructor(titulo, autor, conteudo) {
+      this.titulo = titulo;
+      this.autor = autor;
+      this.conteudo = conteudo;
     }
-}
+  }
+
+  novoPost = new Post();
+
+function aoClicar(){
+  titulo = document.getElementById('titulo').value
+  autor = document.getElementById('autor').value
+  conteudo = document.getElementById('conteudo').value
+
+
+  console.log(titulo,autor,conteudo)
+
+} 
+ 
