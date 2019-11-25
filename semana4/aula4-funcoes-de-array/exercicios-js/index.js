58f6a1678794bd30602fff5926cfef5bf38e8a94
rafacazal <rafaelacazal12@gmail.com>
2019-11-25 00:11:08 -0200

    ex1 ok

---
+++ semana4/aula4-funcoes-de-array/exercicios-js/index.js
@@ -0,0 +1,36 @@
+
+arrayFour = [
+    { nome: "Darvas", idade: 20 },
+    { nome: "Goli", idade: 10 },
+    { nome: "Paulinha", idade: 12 },
+    { nome: "Artur", idade: 89 },
+    { nome: "Soter", idade: 55 },
+    { nome: "Miau", idade: 1 },
+    { nome: "Lu", idade: 32 }
+]
+
+// a. maiores de 20
+
+function soadultos(arrayFour){
+
+    let novoArray = arrayFour.filter((item) => {
+        return item.idade >= 20
+    })
+
+    console.log(novoArray)
+}
+
+soadultos(arrayFour)
+
+//  b. menores de 20
+
+function socrianca(arrayFour){
+
+    let arrayCrianca = arrayFour.filter((item) => {
+        return item.idade <= 20
+    })
+
+    console.log(arrayCrianca)
+}
+
+socrianca(arrayFour)
