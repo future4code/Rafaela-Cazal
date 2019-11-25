94e812df4b3e62e049d38bfd013eb82f3d3720a9
rafacazal <rafaelacazal12@gmail.com>
2019-11-25 00:43:30 -0200

    ex2 ok

--- semana4/aula4-funcoes-de-array/exercicios-js/index.js
+++ semana4/aula4-funcoes-de-array/exercicios-js/index.js
@@ -1,38 +1,71 @@
 
 // exercicio 1
 
-// arrayFour = [
-//     { nome: "Darvas", idade: 20 },
-//     { nome: "Goli", idade: 10 },
-//     { nome: "Paulinha", idade: 12 },
-//     { nome: "Artur", idade: 89 },
-//     { nome: "Soter", idade: 55 },
-//     { nome: "Miau", idade: 1 },
-//     { nome: "Lu", idade: 32 }
-// ]
+arrayFour = [
+    { nome: "Darvas", idade: 20 },
+    { nome: "Goli", idade: 10 },
+    { nome: "Paulinha", idade: 12 },
+    { nome: "Artur", idade: 89 },
+    { nome: "Soter", idade: 55 },
+    { nome: "Miau", idade: 1 },
+    { nome: "Lu", idade: 32 }
+]
 
 // // a. maiores de 20
 
-// function soadultos(arrayFour){
+function soadultos(arrayFour){
 
-//     let novoArray = arrayFour.filter((item) => {
-//         return item.idade >= 20
-//     })
+    let novoArray = arrayFour.filter((item) => {
+        return item.idade >= 20
+    })
 
-//     console.log(novoArray)
-// }
+    console.log(novoArray)
+}
 
-// soadultos(arrayFour)
+soadultos(arrayFour)
 
 // //  b. menores de 20
 
-// function socrianca(arrayFour){
+function socrianca(arrayFour){
 
-//     let arrayCrianca = arrayFour.filter((item) => {
-//         return item.idade <= 20
-//     })
+    let arrayCrianca = arrayFour.filter((item) => {
+        return item.idade <= 20
+    })
 
-//     console.log(arrayCrianca)
-// }
+    console.log(arrayCrianca)
+}
 
-// socrianca(arrayFour)
+socrianca(arrayFour)
+ 
+
+// exercicio 2 
+
+
+const array = [1, 2, 3, 4, 5, 6]
+
+// a.
+function vezesDois(array) {
+
+        array.forEach((element, index, array) => {
+      element[index] *= 2
+    })
+
+}
+// b.
+function vezesTres(array) {
+
+    array.forEach((element, index, array) => {
+      element[index] *= 3
+    })
+
+}
+//  c.
+function definir(array) {
+   array.forEach((element, index, array) => {
+    if (element[index] % 2 == 0) {
+        console.log(element[index] + " é par")
+    } else {
+        console.log(element[index] + " é impar")
+    }
+})
+}
