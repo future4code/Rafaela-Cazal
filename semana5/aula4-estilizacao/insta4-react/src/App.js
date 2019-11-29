0b6582b3e785254b683c80abba0d1d12b72de5ea
rafacazal <rafaelacazal12@gmail.com>
2019-11-29 00:10:16 -0200

    atividade instagram incompleto

---
+++ semana5/aula4-estilizacao/insta4-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import PostSection from './components/PostSection/PostSection';
+
+function App() {
+  return (
+    <div className="App">   
+
+    <h2> Insta4</h2>  
+      <PostSection />
+      <h2> Post </h2>
+    </div>
+  );
+}
+
+
+export default App;
