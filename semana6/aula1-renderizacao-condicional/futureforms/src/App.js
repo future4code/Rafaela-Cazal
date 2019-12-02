ff3d49dce4da1de36ce2627c141272cecdb04515
rafacazal <rafaelacazal12@gmail.com>
2019-12-02 19:21:00 -0200

    futureforms incompleto

--- semana6/aula1-renderizacao-condicional/futureforms/src/App.js
+++ semana6/aula1-renderizacao-condicional/futureforms/src/App.js
@@ -1,34 +1,62 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-import Etapa1 from './components/Etapa1/Etapa1'
 
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-`
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       tela: "inicial",
+      mensagens: 0
     };
   }
+    onClickNext = () => {
+      this.setState({ tela: "etapa2" });
+    };
+    onClickNext2 = () => {
+      this.setState({ tela: "inicial" });
+    };
 
-  onClickLogin = () => {
-    this.setState({ tela: "etapa1" });
-  };
+    render() {
+      const tela =
+        this.state.tela === "inicial" ? (
+          <div>
+          <h2>ETAPA 1 - DADOS GERAIS</h2>
+          <p>1. Qual o seu nome?</p>
+          <input></input>
+          <p>2. Qual sua idade?</p>
+          <input></input> 
+          <p>3. Qual seu email?</p>
+          <input></input> 
+          <p> 4. Qual a sua escolaridade?</p>
+          <select>
+            <option> Ensino médio incompleto </option>
+            <option> Ensino médio completo </option>
+            <option> Ensino superior incompleto </option>
+            <option> Ensino superior completo </option>
+          </select>
+          <button onClick={this.onClickNext}>Próxima Etapa</button>
+        </div> 
+        ): (
+          <div> 
+            <h2> ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h2>
+            <p> 5. Qual curso?</p>
+            <input></input>
+            <p> 6. Qual a unidade de ensino?</p>
+            <input></input>
+            <button onClick={this.onClickNext2}>Próxima Etapa </button>
+          </div>  
+        );
 
-  render() {
-    return (
-      <Container>
-        <h1>FutureForms</h1>
-        <FormularioUm/>
-      </Container>
-    );
+      return (
+        <div className="App">
+          <h1>FutureForms</h1>
+          {tela}
+        </div>
+      );
+    }
   }
-}
-
 
 export default App;
+
+
--- semana6/aula1-renderizacao-condicional/futureforms/src/components/Etapa1/Etapa1.js
+++ semana6/aula1-renderizacao-condicional/futureforms/src/components/Etapa1/Etapa1.js
@@ -2,38 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const FormularioUm = styled.div`
-display: flex;
-flex-direction: column;
-`
-
-const PrimeiroInput = styled.input`
-width: 20px
-`
-
-class Etapa1 extends React.Component {
-    constructor(props) {
-      super(props);
+// const FormularioUm = styled.div`
+// display: flex;
+// flex-direction: column;
+// `
+// class Etapa1 extends React.Component {
+//     constructor(props) {
+//       super(props);
   
-      this.state = {
-        tela: "etapa1",
-      };
-    }
+//       this.state = {
+//         tela: "etapa1",
+//       };
+//     }
   
-    onClickProximaEtapa = () => {
-      this.setState({ tela: "etapa2" });
-    };
+//     onClickProximaEtapa = () => {
+//       this.setState({ tela: "etapa2" });
+//     };
   
-    render() {
-      return (
-        <FormularioUm>
-          <h1>ETAPA 1 - DADOS GERAIS </h1>
-          <label> 1. Qual o seu nome?</label>
-          <PrimeiroInput type="text"> </PrimeiroInput>
-        </FormularioUm>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <FormularioUm>
+//           <h2>ETAPA 1 - DADOS GERAIS </h2>
+//           <label> 1. Qual o seu nome?</label>
+//           <input type="text"> </input>
+//         </FormularioUm>
+//       );
+//     }
+//   }
   
   export default Etapa1;
   
