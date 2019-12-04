865638a117f35e20371b73d07a1ec10f306ba353
rafacazal <rafaelacazal12@gmail.com>
2019-12-04 19:09:52 -0200

    atividade obrigatoria incompleta

--- semana6/aula2-lifecycle/lista-de-tarefas/src/App.js
+++ semana6/aula2-lifecycle/lista-de-tarefas/src/App.js
@@ -1,34 +1,68 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 500px;
+  margin-left: 400px
+`
+const ListaContainer = styled.div`
+margin-top: 10px;
+`
+const InputTarefa = styled.input`
+margin-right: 10px;
+`
+
+const ContainerDaNovaTarefa = styled.section`
+margin-top: 10px;
+`
 
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      valorDaTarefa: "",
-      filtro: ""
+      valorTarefa: '',
+      arrayTarefas: [],
     }
   }
 
-  valorTarefa = (e) => {
-    this.setState({ valorDaTarefa: e.target.value })
+  onChangeTarefa = (e) => {
+    this.setState({ valorTarefa: e.target.value })
+    console.log(this.state.valorTarefa)
   }
 
-  criarNovaTarefa = () => {
-    const tarefa = {
-      inputDaTarefa : this.state.valorDaTarefa,
+  aoClicarAdicionar = (props) => {
+    if(this.state.valorTarefa !== ""){
+      const arrayTarefasCopy = [...this.state.arrayTarefas, props]
+      this.setState({
+        arrayTarefas: arrayTarefasCopy,
+        valorTarefa: '',
+      });
+    } else {
+      alert("Tarefa não deve ficar vazia")
     }
+  }
 
-render() {
-  return (
-    <div>
-      <h1> Lista de tarefas</h1>
- <input value={this.state.valorDaTarefa} onChange={this.valorTarefa}></input>
- <button onClick={criarNovaTarefa(tarefa)} >Adicionar</button>
-    </div>
-  );
-}
+
+  render() {
+    return (
+      <MainContainer>
+        <h1>Lista de Tarefas</h1>
+        <ListaContainer> 
+          <InputTarefa type='text' value={this.state.valorTarefa} onChange={this.onChangeTarefa} placeholder='Inserir tarefa'/>
+          <button onClick={this.aoClicarAdicionar}>Adicionar</button> 
+          </ListaContainer> 
+          <ContainerDaNovaTarefa>
+          {this.state.arrayTarefas.map((props) => 
+          <li><strong>{this.props.valorTarefa}</strong></li>)}
+        </ContainerDaNovaTarefa>
+    </MainContainer>
+    );
+  }
 }
 
 export default App;
