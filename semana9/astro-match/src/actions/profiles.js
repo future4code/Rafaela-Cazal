f030a892b7653eb9f11912db01405dec65c102e4
rafacazal <rafaelacazal12@gmail.com>
2020-01-11 11:39:26 -0300

    astro match

---
+++ semana9/astro-match/src/actions/profiles.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export const clearSwipes = () => async (dispatch) => {
+	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/rafacazal/clear')
+}
+
---
+++ semana9/astro-match/src/reducers/profiles.js
@@ -0,0 +1,7 @@
+const initialState = null
+
+const profiles = (state = initialState, action) => {
+  return state
+}
+
+export default profiles
