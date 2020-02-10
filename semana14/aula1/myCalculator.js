b354ca468b2e96aa303cd0697504089b6c7627c5
rafacazal <rafaelacazal12@gmail.com>
2020-02-10 15:16:38 -0300

    commit para resolucao

---
+++ semana14/aula1/myCalculator.js
@@ -0,0 +1,13 @@
+//  exercicio 1 
+const numUm = Number(process.argv[3]);
+const numDois = Number(process.argv[4]);
+
+const add = numUm + numDois;
+
+const sub = numUm - numDois;
+
+const mult = numUm * numDois;
+
+const div = numUm / numDois;
+
+console.log("Resposta: ", add)
