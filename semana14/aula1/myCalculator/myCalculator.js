24cfbffaa3c12232fa60c278b5c6af77108725f6
rafacazal <rafaelacazal12@gmail.com>
2020-02-10 21:26:47 -0300

    desafio 2

--- semana14/aula1/myCalculator/myCalculator.js
+++ semana14/aula1/myCalculator/myCalculator.js
@@ -3,6 +3,8 @@ const calculate = process.argv[2]
 const num1 = Number(process.argv[3]);
 const num2 = Number(process.argv[4]);
 let totalValue = 0;
+const red   = '\u001b[31m';
+const green = '\033[0;32m';
 
 switch(calculate){
     case "add":
@@ -17,7 +19,7 @@ switch(calculate){
     case "div":
         totalValue = num1 / num2
         break
-    default: console.log( " operação não encontrada")
+    default: console.log( `${red} Operação não encontrada`)
 }
 
-console.log("Resposta: ", totalValue)
+console.log(`${green}Resposta: ${totalValue}`)
