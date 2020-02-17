114eae4c380d394a6619751496ef3602d6472366
rafacazal <rafaelacazal12@gmail.com>
2020-02-16 22:30:59 -0300

    tentativa de resolucao

---
+++ semana14/projeto-f4bank/projeto-f4bank/src/createAccount.ts
@@ -0,0 +1,4 @@
+
+const userName = process.argv[4]
+const userCPf = process.argv[5]
+const userBirthDate = process.argv[6]
---
+++ semana14/projeto-f4bank/projeto-f4bank/src/types.ts
@@ -0,0 +1,13 @@
+
+type account = {
+    name: string,
+    cpf: string,
+    birthDate: string
+}
+
+
+ type transactions = {
+    value: number,
+    date: string,
+    description: string
+  } 
